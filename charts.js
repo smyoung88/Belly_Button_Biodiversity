bed093efff433ed08af716c919cac6fe5ac68f3b
Sam Young <samyoung@deepimaging.com>
2021-05-24 01:11:48 -0600

    bar chart checkpoint

--- charts.js
+++ charts.js
@@ -54,24 +54,24 @@ function buildMetadata(sample) {
   });
 }
 
-// // 1. Create the buildCharts function.
-// function buildCharts(sample) {
-//   // 2. Use d3.json to load and retrieve the samples.json file 
-//   d3.json("samples.json").then((data) => {
-//     // 3. Create a variable that holds the samples array. 
-//     var samples = data.samples;
-//     // 4. Create a variable that filters the samples for the object with the desired sample number.
-//     var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
-//     //  5. Create a variable that holds the first sample in the array.
-//     var result = resultArray[0];
-//     var PANEL = d3.select("#bar");
-//     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
-//     var otu_ids = result.otu_ids;
-//     var otu_labels = result.otu_labels;
-//     var sample_values = result.sample_values;
-//     // 7. Create the yticks for the bar chart.
-//     // Hint: Get the the top 10 otu_ids and map them in descending order  
-//     //  so the otu_ids with the most bacteria are last. 
+// 1. Create the buildCharts function.
+function buildCharts(sample) {
+  // 2. Use d3.json to load and retrieve the samples.json file 
+  d3.json("samples.json").then((data) => {
+    // 3. Create a variable that holds the samples array. 
+    var samples = data.samples;
+    // 4. Create a variable that filters the samples for the object with the desired sample number.
+    var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
+    //  5. Create a variable that holds the first sample in the array.
+    var result = resultArray[0];
+    var PANEL = d3.select("#bar");
+    // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
+    var otu_ids = result.otu_ids;
+    var otu_labels = result.otu_labels;
+    var sample_values = result.sample_values;
+    // 7. Create the yticks for the bar chart.
+    // Hint: Get the the top 10 otu_ids and map them in descending order  
+    //  so the otu_ids with the most bacteria are last. 
 
 //     var newOtuIds = otu_ids.slice(0,10).reverse().map(String);
 //     var yticks = []
